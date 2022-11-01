c662ae48214f16ddfbc7550ac083e07e35ca016a
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-01 20:17:49 -0300

    feat: route for Driver component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from "./components/home/home.component";
+import { DriverListComponent } from "./components/driver-list/driver-list.component";
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'driver', component: DriverListComponent }
     ]
   }
 ];
