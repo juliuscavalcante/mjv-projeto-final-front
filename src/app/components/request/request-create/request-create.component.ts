b87e8d0034afa22fb387a1307f3a49d346fffa15
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-03 08:54:46 -0300

    feat: form validation for Request create

--- src/app/components/request/request-create/request-create.component.ts
+++ src/app/components/request/request-create/request-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-request-create',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestCreateComponent implements OnInit {
 
+  priority: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  title: FormControl = new FormControl(null, [Validators.required])
+  description: FormControl = new FormControl(null, [Validators.required])
+  engineer: FormControl = new FormControl(null, [Validators.required])
+  mechanic: FormControl = new FormControl(null, [Validators.required])
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fieldValidation(): boolean {
+    return this.priority.valid &&
+        this.status.valid &&
+        this.title.valid &&
+        this.description.valid &&
+        this.engineer.valid &&
+        this.mechanic.valid
   }
 
 }
