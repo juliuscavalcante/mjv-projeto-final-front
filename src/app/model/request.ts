256a86cb33ba017a4f0852a6c327c03fdfd6f4e1
Julius Cavalcante <julius.c@outlook.com.br>
2022-11-03 08:06:31 -0300

    feat: creation of Request interface

---
+++ src/app/model/request.ts
@@ -0,0 +1,13 @@
+export interface Request {
+    id?: any;
+    openingDate?: string;
+    closingDate?: string;
+    priority: string;
+    status: string
+    title: string;
+    description: string;
+    technician: any;
+    customer: any;
+    engineerName: string;
+    mechanicName: string;
+}
